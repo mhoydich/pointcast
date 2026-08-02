d6974b065b98ea05bc4ff7756aab38276e2ccf51
Mike Hoydich <mhoydich@gmail.com>
2026-08-02 14:17:50 -0700

    Publish OPEN/25 outdoor commons

--- src/pages/sitemap-discovery.xml.ts
+++ src/pages/sitemap-discovery.xml.ts
@@ -196,6 +196,8 @@ const staticUrls: SitemapEntry[] = [
   ['https://pointcast.xyz/beach-commons/v18/skills.json', 'weekly', '0.96'],
   ['https://pointcast.xyz/beach-commons/v18/engineering', 'weekly', '0.99'],
   ['https://pointcast.xyz/beach-commons/v18/engineering.json', 'weekly', '0.96'],
+  ['https://pointcast.xyz/beach-commons/v18/outdoors', 'weekly', '0.99'],
+  ['https://pointcast.xyz/beach-commons/v18/outdoors.json', 'weekly', '0.96'],
   ['https://pointcast.xyz/reviews', 'weekly', '0.92'],
   ['https://pointcast.xyz/reviews.json', 'weekly', '0.88'],
   ['https://pointcast.xyz/crystal-ball-pass', 'weekly', '0.99'],
